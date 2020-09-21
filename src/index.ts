e82735b959ca3c55a319f79939cb8d540bd91947
Ernie Hayter <Ernie.Hayter@k2.com>
2020-09-21 15:49:53 +0100

    Content

--- src/index.ts
+++ src/index.ts
@@ -62,7 +62,7 @@ function onexecuteLinesSplit(properties: SingleRecord): Promise<void> {
         // xhr.onreadystatechange = function() {
             try {
                 postResult({
-                    "line": JSON.stringify(properties["file"]),
+                    "line": JSON.stringify(properties["file"].content),
                     "output": properties["file"]
                 });
                 resolve();
